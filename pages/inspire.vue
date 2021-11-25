b83c332888891f7b9742330825d53f606fc70f35
amy <ctrlalternativedelete@gmail.com>
2021-11-26 09:05:17 +1100

    added abcjs

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,19 +1,40 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+    <div id="inspire">
+        <div ref="renderer" />
+        <div class="button-grid">
+          <button class="reroll" v-for="note in notes" :key="note">{{note}}</button>
+        </div>
+    </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import abcjs, {TuneObjectArray, AbcVisualParams} from "abcjs";
+
+export default Vue.extend({
+
+    data() {
+        return {
+            noteRenderOptions: {
+                responsive: "resize",
+                staffwidth: 400,
+                add_classes: true
+            } as AbcVisualParams,
+            noteRenderString: "dDAA|",
+            notes: ["C", "CD", "D", "DE", "E", "F", "FG", "G", "GA", "A", "AB", "B"],
+            octaves: Array.from({length: 10}, (_, i) => i + 1)
+        }
+    },
+
+    mounted() {
+        
+        this.$data.octaves
+        abcjs.renderAbc(this.$refs.renderer as HTMLElement, this.$data.noteRenderString, this.$data.noteRenderOptions)
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
