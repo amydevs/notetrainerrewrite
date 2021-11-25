7e605e63820c3203279d5714efc1a9e2c95c4c8f
amy <ctrlalternativedelete@gmail.com>
2021-11-26 10:23:28 +1100

    added vexflow

---
+++ components/NoteRender.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="render" :id="dark.toString()"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  computed: {
+    dark() {
+      return this.$vuetify.theme.dark;
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+#true {
+  --color-var: white;
+}
+#false {
+  --color-var: black;
+}
+.render {
+  ::v-deep svg {
+    * {
+      stroke: var(--color-var) !important;
+      fill: var(--color-var) !important;
+    }
+  }
+}
+</style>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="inspire">
-        <div ref="renderer" />
+        <NoteRenderer ref="noterender" />
         <div class="button-grid">
           <button class="reroll" v-for="note in notes" :key="note">{{note}}</button>
         </div>
@@ -10,17 +10,16 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import abcjs, {TuneObjectArray, AbcVisualParams} from "abcjs";
+import Vex from 'vexflow';
+
+import NoteRenderer from "~/components/NoteRender.vue";
 
 export default Vue.extend({
-
+    components: {
+        NoteRenderer
+    },
     data() {
         return {
-            noteRenderOptions: {
-                responsive: "resize",
-                staffwidth: 400,
-                add_classes: true
-            } as AbcVisualParams,
             noteRenderString: "dDAA|",
             notes: ["C", "CD", "D", "DE", "E", "F", "FG", "G", "GA", "A", "AB", "B"],
             octaves: Array.from({length: 10}, (_, i) => i + 1)
@@ -29,8 +28,25 @@ export default Vue.extend({
 
     mounted() {
         
-        this.$data.octaves
-        abcjs.renderAbc(this.$refs.renderer as HTMLElement, this.$data.noteRenderString, this.$data.noteRenderOptions)
+        console.log(this.$refs['noterender'] as HTMLElement)
+        const VF = Vex.Flow;
+        // Create a VexFlow renderer attaced to the DIV element "boo"
+        var renderer = new VF.Renderer((this.$refs['noterender'] as any)?.$el, VF.Renderer.Backends.SVG)
+        
+        // Configure the rendering context.
+        renderer.resize(500, 500);
+        const context = renderer.getContext();
+        context.setFont("Arial", 10, 10).setBackgroundFillStyle("#eed");
+
+        // Create a stave of width 400 at position 10, 40 on the canvas.
+        const stave = new VF.Stave(10, 40, 400);
+
+        // Add a clef and time signature.
+        stave.addClef("treble").addTimeSignature("4/4");
+
+        // Connect it to the rendering context and draw!
+        stave.setContext(context).draw();
+                
     }
 })
 </script>
